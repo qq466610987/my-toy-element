8fcd9573f15c7604803b7f99ae384547dde2c718
LM0514 <466610987@qq.com>
2024-10-19 15:23:38 +0800

    项目打包

--- packages/components/Icon/Icon.vue
+++ packages/components/Icon/Icon.vue
@@ -20,4 +20,6 @@ const filterProps = computed(() => omit(props, ['type', 'color']))
 const customStyles = computed(() => ({ color: props.color ?? void 0 }));
 </script>
 
-<style scoped></style>
+<style>
+@import './style.css';
+</style>
--- packages/play/src/main.ts
+++ packages/play/src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import ToyElement from 'toy-element'
-import "@toy-element/theme/index.css"
+import "toy-element/dist/index.css"
 
 createApp(App)
 .use(ToyElement)
