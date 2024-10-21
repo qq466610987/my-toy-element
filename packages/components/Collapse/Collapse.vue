3da4a73023fc737c0fd407de00cc9fb4ac88cd32
LM0514 <466610987@qq.com>
2024-10-21 20:34:58 +0800

    1

---
+++ packages/components/Collapse/Collapse.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="er-collapse">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { COLLAPSE_CTX_KEY } from "./constants";
+import type { CollapseProps, CollapseEmits, CollapseContext, CollapseItemName } from "./types";
+import { provide, ref } from "vue";
+
+defineOptions({
+  name: "ErCollapse",
+});
+const props = defineProps<CollapseProps>();
+const emit = defineEmits<CollapseEmits>();
+const activeNames = ref(props.modelValue);
+
+if (props.accordion && activeNames.value.length > 1) {
+  console.warn("accordion mode must set only one active item");
+}
+
+function handleItemClick(name: CollapseItemName) {
+  //todo  
+}
+
+provide(COLLAPSE_CTX_KEY, {
+  activeNames,
+  handleItemClick,
+});
+</script>
---
+++ packages/components/Collapse/CollapseItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="er-collapse-item">
+    <div class="er-collapse-item__header">
+      <span class="er-collapse-item__title">{{ props.title }}</span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { CollapseItemProps } from "./types";
+
+defineOptions({
+  name: "ErCollapseItem",
+});
+
+const props = defineProps<CollapseItemProps>();
+</script>
